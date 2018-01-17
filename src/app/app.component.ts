fec32db02592667eac1aca4c5c5bdb79feeaa4c4
sriram15 <sriram.thiagarajan94@gmail.com>
2018-01-17 23:44:01 +0530

    Added Google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
+import { Router, NavigationEnd } from '@angular/router';
 import { Component } from '@angular/core';
 import {NgsRevealConfig} from 'ng-scrollreveal';
 
+declare let ga: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +10,17 @@ import {NgsRevealConfig} from 'ng-scrollreveal';
 })
 export class AppComponent {
   title = 'app';
-  constructor(config: NgsRevealConfig) {
+  constructor(config: NgsRevealConfig,private router: Router) {
     // customize default values of ng-scrollreveal directives used by this component tree
     config.duration = 1000;
     config.easing = 'cubic-bezier(0.645, 0.045, 0.355, 1)';
 
-    //other options here
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        console.log(event.urlAfterRedirects);
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+      }
+    });
   }
 }
