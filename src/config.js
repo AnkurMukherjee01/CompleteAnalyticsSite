2aa6f07cab70023a6f5e42c39cd3c72fc5c6d0e8
sriram15 <sriram.thiagarajan94@gmail.com>
2018-01-23 00:50:41 +0530

    Changing refresh token

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     mailUser: 'completeanalytics@gmail.com',
     clientId: '117242968434-rks42a00n1buek1cgoof3ggpmtagt87d.apps.googleusercontent.com',
     clientSecret: 'DAQAtg_2ckffEKaf-4p_1Pef',
-    refreshToken : '1/kQp5VfuOeBudoVAUueNwKWiTiiN1vvSeWx2ZwS4nC8c',
-    accessToken: 'ya29.GlsPBay1NfnhYfPgAQwLOisHCmJRqUSkx1kwPVyMJkznMdmnQoVfE98NaDHWZ_eZyQdCDelmSdmt_YOOVJL_xBWD5gjzqVbyiVR-j9BVTxDYJAsoxp-KSsEM2K4u'
+    refreshToken : '1/nqzN_6j5UQR7I1wy8Jdb9EeMeA24LcWKCQ9_LST_LaY',
+    accessToken: 'ya29.GltKBbfsmzZ8d884BpBuF8suNZHL6orCNpCovpWHe5Ok3hERqg4ZWUhviRYROZYIkoCmFYJ44Yifz-FeKz-8IdliP2L4Ge1w48QMttrO2db0kaNPjPOiN6xNiPsS'
 }
